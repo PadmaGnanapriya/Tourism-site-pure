f582e047ef63523601cfec6aafed36f6c54bdc50
PadmaGnanapriya <padmaisuru@gmail.com>
2020-03-31 05:11:05 +0530

    finist user data showing page

--- animation.js
+++ animation.js
@@ -24,15 +24,21 @@ date=2200-02-22&numberOfGuests=2&meals=yesMeals&balcony=yes
 
 ----*/
 
-// function getUrlVars() {
-//     var vars = {};
-//     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-//         vars[key] = value;
-//     });
-//     return vars;
-//     console.log(vars);
-//     console.log(value);
-// }
-
-// getUrlVars()
+window.onload = function what(){
+    var PRINTINGDATA='';
+    var vars = {};
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+            vars[key] = value;
+        });
+        if(vars.balcony==='yes#marginz'){
+            vars.balcony='Yes';
+        } else if(vars.balcony==='no#marginz'){
+            vars.balcony='No';
+        }
+        var TITLEDATA= '<table style="width:100%"> <tr><th>username</th> <th>email</th> <th>hottel G.</th><th>date</th> <th>No of Guests</th><th>Balcony</th>  </tr>'   
+        var URLDATA =`<tr> <td>${vars.username}</td> <td>${vars.email}</td> <td>${vars.hottel}</td> <td>${vars.date}</td> <td>${vars.numberOfGuests}</td> <td>${vars.balcony}</td> </tr></table>`
+        var PRINTDATA=TITLEDATA+URLDATA;
+        document.getElementById("user-data").innerHTML = PRINTDATA  
+        return PRINTINGDATA;
+    };
 
