1a6cad0218263d04c2d17e894e621a77affe66a2
PadmaGnanapriya <padmaisuru@gmail.com>
2020-03-31 04:56:34 +0530

    make last data record shown area

--- animation.js
+++ animation.js
@@ -15,3 +15,24 @@ function  navifunction(){
     return x;
 }
 
+
+/*-----Booking detail---------
+
+file:///H:/A_Padma_Codes/Web_Design/Tourism-site-pure/booking.html
+?username=Padme&email=padmaisuru%40gmail.com&hottel=1&
+date=2200-02-22&numberOfGuests=2&meals=yesMeals&balcony=yes
+
+----*/
+
+// function getUrlVars() {
+//     var vars = {};
+//     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+//         vars[key] = value;
+//     });
+//     return vars;
+//     console.log(vars);
+//     console.log(value);
+// }
+
+// getUrlVars()
+
